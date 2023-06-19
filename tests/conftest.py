705b8bc8791f358806866152265cafa698c888c4
ductin <nguyenbaduc.tin@gmail.com>
2023-06-19 23:26:53 +0900

    allow pytest to be called from project base directory as  issue #24

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,28 @@
 import shutil
-import time
+import os, sys
 from pathlib import Path
 
 
 __author__ = "Duc Tin"
 
+# extract needed paths
+original_cwd = Path.cwd()
+tests_folder = Path(__file__).parent
+tests_path = tests_folder.absolute().as_posix()
+lib_path = tests_folder.parent.absolute().as_posix()
 
-log = Path.cwd() / 'logs/log.txt'
+# set environment variable PYTHONPATH with correct paths
+# so that any subprocess can file the logger-tt while still giving the log file at the correct location
+python_path = os.environ.get("PYTHONPATH", "") + os.pathsep
+python_path += os.pathsep.join([lib_path, tests_path])
+os.environ["PYTHONPATH"] = python_path.strip(os.pathsep)
+if sys.version.startswith('3.6'):
+    # don't know why but python 3.6 need this
+    sys.path.insert(0, lib_path)
+
+# set working directory to the "tests" folder
+os.chdir(tests_folder)
+log = tests_folder / 'logs/log.txt'
 
 
 def remove_log():
@@ -49,7 +65,7 @@ def pytest_sessionfinish(session, exitstatus):
     Called after whole test run finished, right before
     returning the exit status to the system.
     """
-    pass
+    os.chdir(original_cwd)
 
 
 def pytest_unconfigure(config):

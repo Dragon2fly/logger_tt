edc9bad2dd82cc64bcf4b6fce50b6f9a666f9eb0
ductin <nguyenbaduc.tin@gmail.com>
2025-02-04 11:51:16 +0900

    reformat code style

--- tests/test_recur_exception.py
+++ tests/test_recur_exception.py
@@ -6,7 +6,7 @@ from subprocess import run
 __author__ = "ZeroRin"
 log = Path.cwd() / 'logs/log.txt'
 
-TRACE=r'''Traceback \(most recent call last\):
+TRACE = r'''Traceback \(most recent call last\):
   File ".+?", line \d+?, in foo
     raise RuntimeError\(a\)
      |-> a = 1
@@ -31,12 +31,14 @@ Traceback \(most recent call last\):
      |-> c = 3
 RuntimeError: 3'''
 
+
 def test_recur_exception_caught():
     cmd = [sys.executable, "exception_on_exception.py", "caught"]
     run(cmd)
     data = log.read_text()
     pattern = rf'Caught exception\n{TRACE}'
     assert re.search(pattern, data, re.DOTALL)
+
 
 def test_recur_exception_uncaught():
     cmd = [sys.executable, "exception_on_exception.py", "uncaught"]
